b09331382677cd28a9d00b2af27fbf041f7e3fcc
SnO2WMaN <me@sno2wman.net>
2022-07-01 16:14:01 +0900

    一応wasm-packでビルド出来る形になっているはず

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     devshell.url = "github:numtide/devshell";
-
+    rust-overlay.url = "github:oxalica/rust-overlay";
     flake-utils.url = "github:numtide/flake-utils";
     flake-compat = {
       url = "github:edolstra/flake-compat";
@@ -13,6 +13,7 @@
   outputs = {
     self,
     nixpkgs,
+    rust-overlay,
     flake-utils,
     devshell,
     ...
@@ -23,12 +24,20 @@
           inherit system;
           overlays = [
             devshell.overlay
+            rust-overlay.overlays.default
           ];
         };
       in {
         devShell = pkgs.devshell.mkShell {
           imports = [
             (pkgs.devshell.importTOML ./devshell.toml)
+          ];
+          packages = with pkgs; [
+            # https://gist.github.com/573/885a062ca49d2db355c22004cc395066?permalink_comment_id=3922859#gistcomment-3922859
+            (rust-bin.stable.latest.default.override {
+              extensions = ["rust-src"];
+              targets = ["wasm32-unknown-unknown"];
+            })
           ];
         };
       }
